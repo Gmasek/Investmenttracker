322489b333c4f4b6ee49dc66e1c332138f469d92
Gmasek <masek.gergely98@gmail.com>
2024-06-02 10:42:33 +0200

    api small fix

--- backend/api/helpers/get_datafromApi.py
+++ backend/api/helpers/get_datafromApi.py
@@ -9,4 +9,4 @@ def getSimplePricedata(ticker:str,daysback=30,column="Open")->list:
     stock_data = yf.download(ticker,start=(curr_date-timedelta(daysback)),end=curr_date)
     stock_data = pd.DataFrame(stock_data)
     stock_data[column] = np.round(stock_data[column],3)
-    return pd.array(stock_data[column])
+    return pd.array(stock_data[column]).astype(float)
