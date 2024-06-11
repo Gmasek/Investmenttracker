13b1568190445c17143ba79132343ac183485271
Gmasek <masek.gergely98@gmail.com>
2024-06-11 12:07:09 +0200

    movement across login logout implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,9 @@ function App() {
           </ProtectedRoute>
         }
       />
-      <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<Login />}>
+       
+      </Route>
       <Route path="/logout" element={<Logout />} />
       <Route path="/register" element={<RegisterAndLogout />} />
       <Route path="*" element={<NotFound />}></Route>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,23 @@
 import Form from "../components/Form"
+import { useNavigate } from "react-router-dom"
+
 
 function Login() {
-    return <Form route="/api/token/" method="login" />
+    const navigate = useNavigate()
+
+    return <>
+        <Form route="/api/token/" method="login" />
+        <br/>
+        <div className="text-center">
+            <h1 className="text-2xl p-5">Dont have an acocunt yet ? </h1>
+            <button onClick={() => navigate("/register")}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >Register</button>
+        </div>
+        
+    </>
+    
+
 }
 
 export default Login
